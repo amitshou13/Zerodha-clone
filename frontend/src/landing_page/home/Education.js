84bfe468032b5a403fe124b83ced0948b6cabba2
amitshou13 <shouamit13@gmail.com>
2025-01-17 00:18:02 +0530

    Updated home page

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Education() {
     return (
-        <h1> Hello Hi How are you</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src='media/images/education.svg' style={{width:"70%"}}></img>
+                </div>
+                <div className='col-6 mt-3'>
+                    <h1 className='mb-3 fs-2'>Free and open market education</h1>
+                    <p>Varsity, the largest online stock market education book in the world <br/> covering everything from the basics to advanced trading.</p>
+                    <a href='' style={{textDecoration:"none"}}>Varsity <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    <p className='mt-4'>TradingQ&A, the most active trading and investment community in <br/>India for all your market related queries.</p>
+                    <a href='' style={{textDecoration:"none"}}>TradingQ&A <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                </div>
+                
+            </div>
+        </div>
     );
 }
 
--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -2,25 +2,30 @@ import React from 'react'
 
 function Stats() {
     return (
-        <div className='container'>
-            <div className='row'>
-                <div className='col-6'>
-                    <h1>Trush with confidence</h1>
+        <div className='container p-3'>
+            <div className='row p-5'>
+                <div className='col-6 p-5'>
+                    <h1 className='fs-2 mb-5'>Trush with confidence</h1>
 
-                    <h2>Customer-first always</h2>
-                    <p>That's why 1.5+ crore customers trust Zerodha with ₹4.5+ lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+                    <h2 className='fs-4 mt-3'>Customer-first always</h2>
+                    <p className='text-muted mt-2'>That's why 1.5+ crore customers trust Zerodha with <br/> ₹4.5+ lakh crores of equity investments and contribute to <br/> 15% of daily retail exchange volumes in India.</p>
 
-                    <h2>No spam or gimmicks</h2>
-                    <p>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
+                    <h2 className='fs-4 mt-4'>No spam or gimmicks</h2>
+                    <p className='text-muted mt-2'>No gimmicks, spam, "gamification", or annoying push <br/> notifications. High quality apps that you use at your <br/> pace, the way you like.</p>
 
-                    <h2>The Zerodha universe</h2>
-                    <p>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+                    <h2 className='fs-4 mt-4'>The Zerodha universe</h2>
+                    <p className='text-muted mt-2'>Not just an app, but a whole ecosystem. Our investments <br/> in 30+ fintech startups offer you tailored services <br/> specific to your needs.</p>
 
-                    <h2>Do better with money</h2>
-                    <p>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+                    <h2 className='fs-4 mt-4'>Do better with money</h2>
+                    <p className='text-muted mt-2'>With initiatives like Nudge and Kill Switch, we don't just <br/> facilitate transactions, but actively help you do better <br/> with your money.</p>
+                    
                 </div>
-                <div className='col-6'>
-                    <img src='media/images/ecosystem.png' style={{width:"75%"}}></img>
+                <div className='col-6 p-5'>
+                    <img src='media/images/ecosystem.png' style={{width:"100%"}} />
+                    <div className='text-center'>
+                        <a href='' className='mx-5' style={{textDecoration:"none"}}>Explore our products <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                        <a href=''  style={{textDecoration:"none"}}>Try kite demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+                    </div>
                 </div>
             </div>
         </div>
