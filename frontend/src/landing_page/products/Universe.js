e7326ef4f8c309881b2548cbd47fe676086108a7
amitshou13 <shouamit13@gmail.com>
2025-01-22 00:22:42 +0530

    Updated Product Page

--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -41,23 +41,6 @@ function Universe() {
                     <button className='p-2 btn btn-primary fs-5' style={{width:"17.5%", margin: "0 auto"}} >Sign up for free</button>
                 </div>
             </div>
-
-
-{/*             
-            <div className='row text-center p-5'>
-                <div className='col-4 p-5'>
-                    <img src='media/images/zerodhaFundhouse.png' style={{width:"70%"}}/>
-                    <p className='text-small text-muted mt-3'>Our asset management venture that is creating simple and transparent index funds to help you save for your goals.</p>
-                </div>
-                <div className='col-4 p-5'>
-                    <img src='media/images/sensibullLogo.svg' style={{width:"70%"}}/>
-                    <p className='text-small text-muted mt-4 pt-3'>Options trading platform that lets you create strategies, analyze positions, and examine data points like open interest, FII/DII, and more.</p>
-                </div>
-                <div className='col-4 p-5'>
-                    <img src='media/images/tijori.svg' style={{width:"50%"}}/>
-                    <p className='text-small text-muted mt-3'>Investment research platform that offers detailed insights on stocks, sectors, supply chains, and more.</p>
-                </div>
-            </div> */}
         </div>
     );
 }
